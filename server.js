6a0d5fc743037a336176e04319ab07b90eb6eb2a
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-11-17 11:06:27 -0800

    server + basic list

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req, res) => {
+    res.send('Connected');
+});
+
+
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
