659e637013386e57f9efbdcf51a30bed517d88c8
ian-mackenzie <ian@mackenzie.work>
2018-11-18 03:25:00 -0800

    adding map inprog

--- client/src/list.js
+++ client/src/list.js
@@ -26,9 +26,80 @@ class List extends Component {
     };
   }
 
+  initAutocomplete = () => {
+    const google = window.google;
+    console.log(this.refs['google-map']);
+    var map = new google.maps.Map(this.refs['google-map'], {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
+    });
+    console.log(map);
+
+    // Create the search box and link it to the UI element.
+    var input = this.refs['pack-input'];
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+
   // Fetch the list on first mount
   componentDidMount() {
     this.getList();
+    this.initAutocomplete();
   }
 
   // Retrieves the list of items from the Express app
@@ -100,11 +171,17 @@ class List extends Component {
               </FormGroup>
               <Button>Submit</Button>
             </Form>
+            <FormGroup>
+              <Label for="map">Location</Label>
+              <Input id="pac-input" ref="pac-input" className="controls" type="text" placeholder="Search Box"/>
+              <div id="map" ref="google-map"></div>
+            </FormGroup>
           </ListGroup>
         )}
       </div>
     );
   }
 }
+
 
 export default List;
