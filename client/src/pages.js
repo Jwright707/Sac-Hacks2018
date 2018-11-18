de1fbc31fa856d9960ad0cf82228dedff1fee00b
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-11-17 18:14:43 -0800

    added image to card and copyright

--- client/src/pages.js
+++ client/src/pages.js
@@ -1,30 +1,81 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Button } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Row,
+  Col
+} from "reactstrap";
+// import "./pages.css";
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Api Request</h1>
         {/* Link to List.js */}
         <Breadcrumb>
-          <Link to={"./list"}>
-            <BreadcrumbItem>
-              <Button color="primary" size="lg">
-                Create Quest
-              </Button>
-            </BreadcrumbItem>
-          </Link>
+          <Row>
+            <div style={{ display: "flex", justifyContent: "right" }}>
+              <Col xs="6">
+                <Card body className="CardBody">
+                  <CardBody>
+                    <CardImg
+                      top
+                      width="100%"
+                      src={require("./business.jpg")}
+                      alt="Card image cap"
+                    />
 
-          <Link to={"./list2"}>
-            <BreadcrumbItem>
-              <Button color="primary" size="lg">
-                Search Quest
-              </Button>
-            </BreadcrumbItem>
-          </Link>
+                    <CardTitle>Create a Quest</CardTitle>
+                    <CardSubtitle />
+                    <CardText>
+                      Get more people to attend your event today and create a
+                      Quest!
+                    </CardText>
+                  </CardBody>
+                  <Link to={"./list"}>
+                    <BreadcrumbItem>
+                      <Button color="primary" size="lg">
+                        Create Quest
+                      </Button>
+                    </BreadcrumbItem>
+                  </Link>
+                </Card>
+              </Col>
+
+              <Col xs="6">
+                <Card body className="CardBody">
+                  <CardBody>
+                    <CardImg
+                      top
+                      width="100%"
+                      src={require("./vacation.jpg")}
+                      alt="Card image cap"
+                    />
+                    <CardTitle>Search for a Quest</CardTitle>
+                    <CardSubtitle />
+                    <CardText>
+                      Get rewarded to expereiencing life- search for a quest
+                      near you!
+                    </CardText>
+                  </CardBody>
+                  <Link to={"./list2"}>
+                    <BreadcrumbItem>
+                      <Button color="primary" size="lg">
+                        Search Quest
+                      </Button>
+                    </BreadcrumbItem>
+                  </Link>
+                </Card>
+              </Col>
+            </div>
+          </Row>
         </Breadcrumb>
       </div>
     );
